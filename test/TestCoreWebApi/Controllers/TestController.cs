48cd9573401e18cda2a9a0292b7747cb346d6fa0
MageshRamesh-oss <rameshmagesh09@gmail.com>
2021-11-26 23:03:06 +0530

    test commit.

---
+++ test/TestCoreWebApi/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestCoreWebApi.Models;
+
+namespace TestCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        const string StatusSuccess = "Sus0011";
+        const string SuccessMessage = "Successfully Fetched";
+        [HttpGet]
+        public IActionResult GetMethod()
+        {
+            return Ok("Sample Output");
+        }
+        [HttpPost]
+        public IActionResult PostMethod(RequestSample property)
+        {
+            return Ok(new Property {Status= StatusSuccess, Message= SuccessMessage, Data= property });
+        }
+
+    }
+}
