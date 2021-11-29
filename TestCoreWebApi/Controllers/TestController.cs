cdda283dc21b9dc7a3dd3a02346cc2ec7cf4512e
MageshRamesh-oss <rameshmagesh09@gmail.com>
2021-11-29 08:46:52 +0530

    Commit

--- TestCoreWebApi/Controllers/TestController.cs
+++ TestCoreWebApi/Controllers/TestController.cs
@@ -14,15 +14,71 @@ namespace TestCoreWebApi.Controllers
     {
         const string StatusSuccess = "Sus0011";
         const string SuccessMessage = "Successfully Fetched";
+
+        List<PropertyName> propertyNames = new List<PropertyName>()
+        {
+            new PropertyName()
+            {
+                Name="Magesh",id=0,email="magesh@gmail.com"
+            },
+            new PropertyName()
+            {
+                Name="Thiru",id=1,email="Thiru@gmail.com"
+            },
+            new PropertyName()
+            {
+                Name="Venkat",id=2,email="Venkat@gmail.com"
+            },
+            new PropertyName()
+            {
+                Name="Ajith",id=3,email="Ajith@gmail.com"
+            }
+
+        };
         [HttpGet]
         public IActionResult GetMethod()
         {
             return Ok("Sample Output");
         }
+        [HttpGet]
+        [Route("{propertyName}")]
+        public IActionResult GetName(string propertyName)
+        {
+            
+            var b1=propertyNames.Where(x => x.Name == propertyName);
+            if(b1.Any())
+            {
+                return Ok(b1);
+            }
+            else
+            {
+                return NotFound("Please Enter Correct Data");
+            }
+            
+        }
         [HttpPost]
+        [Route("{PostName}")]
+        public IActionResult PostGetMethod(string PostName)
+        {
+            if (ModelState.IsValid)
+            {
+                var b1 = propertyNames.Where(x => x.email == PostName);
+                if (b1.Any())
+                {
+                    return Ok(b1);
+                }
+                else
+                {
+                    return NotFound("No Data Found");
+                }
+            }
+         return Ok();
+        }
+        [HttpPost]
+        
         public IActionResult PostMethod(RequestSample property)
         {
-            return Ok(new Property {Status= StatusSuccess, Message= SuccessMessage, Data= property });
+            return Ok(new Property {Status= StatusSuccess, Message= SuccessMessage, Data= property.secondClasses });
         }
 
     }
---
+++ TestCoreWebApi/Models/PropertyName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCoreWebApi.Models
+{
+    public class PropertyName
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int id { get; set; }
+        [EmailAddress]
+        public string email { get; set; }
+    }
+}
