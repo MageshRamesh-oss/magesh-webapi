651ab4e6047e1881413fcdb61bd0ebeadde29f7c
MageshRamesh-oss <rameshmagesh09@gmail.com>
2021-12-01 20:05:50 +0530

    commit

---
+++ CallingApi/CallingApi/Model/ConsumeEventSync .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+namespace ConsoleEventManager
+{
+    public class ConsumeEventSync
+    {
+        public void GetAllEventData() //Get All Events Records  
+        {
+            using (var client = new WebClient()) //WebClient  
+            {
+                var result = client.DownloadString("https://localhost:5001/api/Test"); //URI  
+                Console.WriteLine( result);
+            }
+        }
+    }
+}
---
+++ CallingApi/CallingApi/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleEventManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeEventSync objsync = new ConsumeEventSync();
+            objsync.GetAllEventData();
+            Console.ReadKey();
+        }
+    }
+}
